a22921cc420ae072e37630fdb5db7279c036e86d
Errornautical <errornautical@proton.me>
2025-09-03 16:24:52 +0530

    chunking for the rag loader

--- rag_loader.py
+++ rag_loader.py
@@ -5,6 +5,12 @@ from rag_store import RAGPaths, RAGStore, ContextAssembler, fields_for_section
 CARDS_DIR = Path("/home/rijul/Gitlaboratory/Context_Engineering_LLM/cards")
 paths = RAGPaths.from_base(CARDS_DIR)
 store = RAGStore(RAGPaths.from_base(CARDS_DIR)).load()
+print("Fields loaded:", len(store.fields_by_name))
+print("Policies:", list(store.policy.keys()))
+print("Abbr:", list(store.abbr.keys()))
+print("Ranges:", list(store.ranges.keys()))
+print("Lexicons:", list(store.lexicons.keys()))
+
 
 target_fields = fields_for_section("history")
 page_tokens = ["symptoms", "duration", "tacroz", "xyzal"]
